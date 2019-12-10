cbcc7ef4a5b1029b05d3fae1ed4c38e0535ea976
Gillian <gillian@yeomans.org.uk>
2019-12-10 01:12:14 +0000

    add stars, first media query, rocket reverse

--- public/main.js
+++ public/main.js
@@ -4,10 +4,11 @@ window.addEventListener("scroll", rocketFly);
 function horizontalScroll(event) {
   //see note here about safari vs other browsers RE where scrolling happens
   //https://www.w3schools.com/jsref/prop_element_scrollleft.asp
-
-  event.preventDefault();
-  document.documentElement.scrollLeft += event.deltaY;
-  document.documentElement.scrollLeft += event.deltaX;
+  if (window.innerWidth > 1000) {
+    event.preventDefault();
+    document.documentElement.scrollLeft += event.deltaY;
+    document.documentElement.scrollLeft += event.deltaX;
+  }
 }
 
 //move rocket when scrolling
@@ -33,6 +34,51 @@ function rocketFly() {
 
   //reset rocketPosition for next movement
   rocketPosition = pixelsToMove;
+}
+
+// Make the element draggable:
+dragElement(rocket);
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos3 = e.clientX;
+    // set the element's new position:
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+
+    //add some more stuff here so page scrolls when you drag the rocket
+    //also need to stop it from going over edge of navline
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
 //for project gallery: descriptions show on hover
@@ -65,7 +111,27 @@ function radioClick(event) {
 }
 
 bios = {
-  short: "this is a short bio",
-  medium: "this is a medium bio",
-  long: "this is a long bio"
+  short:
+    "A shorter bio about me.  TL;DR.  Who I am, what I've done in my life, why I want to be a developer.",
+  medium:
+    "A medium length bio about me.  This will be the default.  Not too much information, not too little.  Who I am, what I've done in my life, why I want to be a developer.",
+  long:
+    "A longer bio about me.  More background and details.  Who I am, what I've done in my life, why I want to be a developer."
 };
+
+//function to autogenerate stars for each section
+function stars() {
+  const sections = document.getElementsByTagName("section");
+  Array.from(sections).forEach(section => {
+    for (let i = 0; i < 30; i++) {
+      let newStar = document.createElement("img");
+      newStar.src = "../temp resources/star.svg";
+      newStar.classList.add("star");
+      newStar.style.top = Math.random() * window.innerHeight + "px";
+      newStar.style.left = Math.random() * window.innerWidth + "px";
+      section.appendChild(newStar);
+    }
+  });
+}
+
+window.onload = stars();
