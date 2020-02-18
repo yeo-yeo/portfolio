cb743fb940f84cac2cf5485b0cb7db0c4c8c0440
Gillian <gillian@yeomans.org.uk>
2020-02-18 00:22:48 +0000

    redo projects loader logic partly

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -20,7 +20,6 @@ const AboutMe = () => {
           onLoad={() => {
             setImageLoaded(true);
           }}
-          // style={{ visibility: imageLoaded ? "visible" : "hidden" }}
         ></img>
         <div className="bio">
           <p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,74 +7,82 @@ import imageChatti from "../assets/projects/chatti.png";
 import imageSignpost from "../assets/projects/week12.jpg";
 
 const Projects = () => {
-  const [showLoader, setShowLoader] = React.useState(true);
+  const [image1Loaded, setImage1Loaded] = React.useState(false);
+  const [image2Loaded, setImage2Loaded] = React.useState(false);
   const [otherProjects] = React.useState(projectList);
-
-  React.useEffect(() => {
-    setTimeout(() => setShowLoader(false), 1500);
-  }, []);
 
   return (
     <>
-      {showLoader && <img id="loader" src={loader} alt="loader"></img>}
-      {!showLoader && (
-        <main>
-          <div className="project-focus">
-            <a href="https://github.com/fac18/chatti">
-              <img
-                className="project-focus-image"
-                src={imageChatti}
-                alt="Chatti app screenshot"
-              ></img>
-            </a>
-            <div className="project-focus-text">
-              <h3>Chatti</h3>
-              <h4>DevOps lead / developer</h4>
-              <h5>React, Express, PostgreSQL, JWT</h5>
-              <p>
-                About the project blah blah blah. About the project blah blah
-                blah. About the project blah blah blah. About the project blah
-                blah blah. About the project blah blah blah. About the project
-                blah blah blah.
-              </p>
-            </div>
-          </div>
-          <div className="project-focus">
-            <a href="https://github.com/fac18/signpost">
-              <img
-                className="project-focus-image"
-                src={imageSignpost}
-                alt="Signpost app screenshot"
-              ></img>
-            </a>
-            <div className="project-focus-text">
-              <h3>Signpost</h3>
-              <h4>QA lead / developer</h4>
-              <h5>React, Express, Airtable, Google Maps, Google Geocoding</h5>
-              <p>
-                About the project blah blah blah. About the project blah blah
-                blah. About the project blah blah blah. About the project blah
-                blah blah. About the project blah blah blah. About the project
-                blah blah blah.
-              </p>
-            </div>
-          </div>
-          <h2>Other projects:</h2>
-          <div className="project-gallery">
-            {otherProjects.map(project => {
-              return (
-                <Project
-                  key={project.title}
-                  title={project.title}
-                  imageUrl={project.imageUrl}
-                  githubUrl={project.githubUrl}
-                  text={project.text}
-                />
-              );
-            })}
-          </div>
-        </main>
+      {!image1Loaded && !image2Loaded && (
+        <img id="loader" src={loader} alt="loader"></img>
       )}
+      <main
+        id="projects-main"
+        style={{
+          visibility: image1Loaded && image2Loaded ? "visible" : "hidden"
+        }}
+      >
+        <div className="project-focus">
+          <a href="https://github.com/fac18/chatti">
+            <img
+              className="project-focus-image"
+              src={imageChatti}
+              alt="Chatti app screenshot"
+              onLoad={() => {
+                setImage1Loaded(true);
+              }}
+            ></img>
+          </a>
+          <div className="project-focus-text">
+            <h3>Chatti</h3>
+            <h4>DevOps lead / developer</h4>
+            <h5>React, Express, PostgreSQL, JWT</h5>
+            <p>
+              About the project blah blah blah. About the project blah blah
+              blah. About the project blah blah blah. About the project blah
+              blah blah. About the project blah blah blah. About the project
+              blah blah blah.
+            </p>
+          </div>
+        </div>
+        <div className="project-focus">
+          <a href="https://github.com/fac18/signpost">
+            <img
+              className="project-focus-image"
+              src={imageSignpost}
+              alt="Signpost app screenshot"
+              onLoad={() => {
+                setImage2Loaded(true);
+              }}
+            ></img>
+          </a>
+          <div className="project-focus-text">
+            <h3>Signpost</h3>
+            <h4>QA lead / developer</h4>
+            <h5>React, Express, Airtable, Google Maps, Google Geocoding</h5>
+            <p>
+              About the project blah blah blah. About the project blah blah
+              blah. About the project blah blah blah. About the project blah
+              blah blah. About the project blah blah blah. About the project
+              blah blah blah.
+            </p>
+          </div>
+        </div>
+        <h2>Other projects:</h2>
+        <div className="project-gallery">
+          {otherProjects.map(project => {
+            return (
+              <Project
+                key={project.title}
+                title={project.title}
+                imageUrl={project.imageUrl}
+                githubUrl={project.githubUrl}
+                text={project.text}
+              />
+            );
+          })}
+        </div>
+      </main>
     </>
   );
 };
