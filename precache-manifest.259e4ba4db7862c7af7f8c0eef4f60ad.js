1a8aee1f9220e83d15dcd2e504f99ab00e75c176
Gillian Yeomans <gillian@cord.com>
2023-11-05 18:34:22 +0000

    Updates

---
+++ precache-manifest.259e4ba4db7862c7af7f8c0eef4f60ad.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d34133f998e4a3164c60f226865fdb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6f32115c8f57f176ccc3",
+    "url": "/portfolio/static/css/main.bd10e1cf.chunk.css"
+  },
+  {
+    "revision": "e12a28ad1e3968b22dd1",
+    "url": "/portfolio/static/js/2.6106c68c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.6106c68c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f32115c8f57f176ccc3",
+    "url": "/portfolio/static/js/main.31e0ed4d.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "ac412d0070da99837de53481416300ec",
+    "url": "/portfolio/static/media/accollo1.ac412d00.png"
+  },
+  {
+    "revision": "599bb36363243a3138c448773f49eb99",
+    "url": "/portfolio/static/media/accollo2.599bb363.png"
+  },
+  {
+    "revision": "ab3f367676fd8724ad54c7cff7217bd3",
+    "url": "/portfolio/static/media/accollo3.ab3f3676.png"
+  },
+  {
+    "revision": "0ef3eb7d7e3e019310b5f11616d91ac1",
+    "url": "/portfolio/static/media/application.0ef3eb7d.png"
+  },
+  {
+    "revision": "dc0e7bc243103e5951fe8fe8b1220b50",
+    "url": "/portfolio/static/media/calculator.dc0e7bc2.jpg"
+  },
+  {
+    "revision": "1764850dce50718968c9d7403e9b8f06",
+    "url": "/portfolio/static/media/chatti.1764850d.png"
+  },
+  {
+    "revision": "76785c2453661d7e115ed634b3566224",
+    "url": "/portfolio/static/media/disagio1.76785c24.png"
+  },
+  {
+    "revision": "ecf4e8554fbc2bcb1b12622c31ceb3eb",
+    "url": "/portfolio/static/media/disagio2.ecf4e855.png"
+  },
+  {
+    "revision": "db7d7929812a64d55b08dd34641660fd",
+    "url": "/portfolio/static/media/disagio3.db7d7929.png"
+  },
+  {
+    "revision": "113cd60f3f16add047f5dae29f4dbd30",
+    "url": "/portfolio/static/media/headshot.113cd60f.jpg"
+  },
+  {
+    "revision": "27af7f092d2ca48c9543a793ad408cdb",
+    "url": "/portfolio/static/media/loader.27af7f09.svg"
+  },
+  {
+    "revision": "90794857498d15c92ce823174aaac1bf",
+    "url": "/portfolio/static/media/tomorrow-latin-400.90794857.woff2"
+  },
+  {
+    "revision": "e3a9267978b84b0f87e7568b5b034cd9",
+    "url": "/portfolio/static/media/tomorrow-latin-400.e3a92679.woff"
+  },
+  {
+    "revision": "8b3a7e7fc446ebc4fe1e4cb704d7a081",
+    "url": "/portfolio/static/media/week12.8b3a7e7f.jpg"
+  },
+  {
+    "revision": "add3f868d627c785c64fd41557500314",
+    "url": "/portfolio/static/media/week2.add3f868.jpg"
+  },
+  {
+    "revision": "2eec4ac81d9e7c87fc99423855f222c2",
+    "url": "/portfolio/static/media/week5.2eec4ac8.jpg"
+  },
+  {
+    "revision": "da651ee8bdbd88a7e3100a11544bfe3b",
+    "url": "/portfolio/static/media/week7.da651ee8.jpg"
+  },
+  {
+    "revision": "39ce44a9f2c580f16e9504e2b24507da",
+    "url": "/portfolio/static/media/week8.39ce44a9.jpg"
+  }
+]);
